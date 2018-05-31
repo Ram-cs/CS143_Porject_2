1919a1a9988742057bf1bf03834251551856ca07
Michael Yu <yu.michael97@gmail.com>
2018-05-31 01:18:42 -0700

    parquet use added

--- Pro2B/reddit_model.py
+++ Pro2B/reddit_model.py
@@ -12,6 +12,7 @@ from pyspark.sql.types import *
 import sys
 from pyspark.sql import Row
 
+import cleantext
 
 #Loading a BZ2 file containing JSON objects into Spark:
 conf = SparkConf().setAppName("CS143 Project 2B")
@@ -22,10 +23,14 @@ sc.addPyFile("cleantext.py")
 # sc = SparkContext.getOrCreate()
 # sqlContext = SQLContext(sc)
 # sc.addPyFile("cleantext.py")
-comments = sqlContext.read.json("/home/cs143/data/comments-minimal.json.bz2") #gives the attibutes and its type
-submissions = sqlContext.read.json("/home/cs143/data/submissions.json.bz2") #gives the attibutes and its type
-labeled_data = sqlContext.read.csv("labeled_data.csv", header=True, mode="DROPMALFORMED")
+# TODO change paths when submitting
+# comments = sqlContext.read.json("/home/cs143/data/comments-minimal.json.bz2") #gives the attibutes and its type
+# submissions = sqlContext.read.json("/home/cs143/data/submissions.json.bz2") #gives the attibutes and its type
+# labeled_data = sqlContext.read.csv("labeled_data.csv", header=True, mode="DROPMALFORMED")
 
+comments = sqlContext.read.parquet("comments")
+submissions = sqlContext.read.parquet("submissions")
+labeled_data = sqlContext.read.parquet("labeled_data")
 
 #TASK 1
 # run spark frame
@@ -63,10 +68,29 @@ def task2():
     
     # csv_table = spark.sql("SELECT * FROM df_table")
 
-    
     query = spark.sql("SELECT data_table.Input_id, data_table.labeldem, data_table.labelgop, data_table.labeldjt, comment_table.body FROM data_table JOIN comment_table ON data_table.Input_id = comment_table.id")
-    query.show()
+    # query.show()
+    query.write.saveAsTable("task2_table")
 
+def connect_all_string(string_list):
+    # str = ' '.join(string_list)
+    # print(str, file=open("output1.txt", "a"))
+    # return str
+    arr = []
+    # unigram
+    arr.append(string_list[1])
+    # bigram
+    arr.append(string_list[2])
+    # trigram
+    arr.append(string_list[3])
+    return arr
+
+#maybe task5 too?
+def task4():
+    spark.udf.register("sanitize", cleantext.sanitize) #UDF
+    spark.udf.register("connect_all_string", connect_all_string)
+
+    spark.sql("SELECT Input_id, connect_all_string(sanitize(comment_body)) AS n_grams  FROM task2_table").show()
 
 
 def main(context):
@@ -74,6 +98,7 @@ def main(context):
     # YOUR CODE HERE
     # YOU MAY ADD OTHER FUNCTIONS AS NEEDED
     task2()
+    task4()
 
 if __name__ == "__main__":
     # comment_laveledData()
