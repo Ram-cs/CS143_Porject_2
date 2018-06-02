c28677599e22304192d31cde1c2b28de0733363d
weimouhe <weimouhe@gmail.com>
2018-06-02 20:23:18 +0800

    complete task 6a and 6b

--- Pro2B/reddit_model.py
+++ Pro2B/reddit_model.py
@@ -6,6 +6,10 @@ from pyspark.sql import SQLContext
 from pyspark.ml.classification import LogisticRegression
 from pyspark.ml.tuning import CrossValidator, ParamGridBuilder
 from pyspark.ml.evaluation import BinaryClassificationEvaluator
+from pyspark.ml.feature import CountVectorizer
+
+from pyspark.sql.types import ArrayType, IntegerType
+from pyspark.sql.functions import col, split
 
 # IMPORT OTHER MODULES HERE
 from pyspark.sql import SparkSession
@@ -29,15 +33,14 @@ sc.addPyFile("cleantext.py")
 # sqlContext = SQLContext(sc)
 # sc.addPyFile("cleantext.py")
 # TODO change paths when submitting
-# comments = sqlContext.read.json("/home/cs143/data/comments-minimal.json.bz2") #gives the attibutes and its type
-# submissions = sqlContext.read.json("/home/cs143/data/submissions.json.bz2") #gives the attibutes and its type
-# labeled_data = sqlContext.read.csv("labeled_data.csv", header=True, mode="DROPMALFORMED")
+#comments = sqlContext.read.json("/home/cs143/data/comments-minimal.json.bz2") #gives the attibutes and its type
+#submissions = sqlContext.read.json("/home/cs143/data/submissions.json.bz2") #gives the attibutes and its type
+#labeled_data = sqlContext.read.csv("labeled_data.csv", header=True, mode="DROPMALFORMED")
 
 # make parquet
-# comments.write.parquet("comments")
-# labeled_data.write.parquet("labeled_data")
-# submissions.write.parquet("submissions")
-
+#comments.write.parquet("comments")
+#labeled_data.write.parquet("labeled_data")
+#submissions.write.parquet("submissions")
 comments = sqlContext.read.parquet("comments")
 submissions = sqlContext.read.parquet("submissions")
 labeled_data = sqlContext.read.parquet("labeled_data")
@@ -78,7 +81,6 @@ def task2():
     # csv_table = spark.sql("SELECT * FROM df_table")
 
     query = spark.sql("SELECT data_table.Input_id, data_table.labeldem, data_table.labelgop, data_table.labeldjt, comment_table.body as comment_body FROM data_table JOIN comment_table ON data_table.Input_id = comment_table.id")
-    # query.show()
     query.write.saveAsTable("task2_table")
 
 
@@ -93,8 +95,8 @@ def connect_all_string(string_list):
     # trigram
     for gram in string_list[3].split():
         arr.append(gram)
-    print(arr, file=open("output1.txt", "a"))
-    return arr
+    #print(arr, file=open("output1.txt", "a"))
+    return ', '.join(arr) #I changed this back to a string of a, b, c, d, e, etc..... Because the form [a,b,c,d,e] is STILL A STRING, not an array.
 
 # maybe task5 too?
 
@@ -103,8 +105,25 @@ def task4():
     spark.udf.register("sanitize", cleantext.sanitize)  # UDF
     spark.udf.register("connect_all_string", connect_all_string)
 
-    spark.sql(
-        "SELECT Input_id, connect_all_string(sanitize(comment_body)) AS n_grams  FROM task2_table").show()
+
+    querytask4 = spark.sql("SELECT Input_id, connect_all_string(sanitize(comment_body)) AS n_grams, labeldjt  FROM task2_table")
+    querytask4.write.saveAsTable("task4_table")
+
+    #querytask4.show()
+
+#task 6A and 6B
+def task6():
+
+    querytask6 = spark.sql("SELECT Input_id,n_grams, IF(labeldjt='1','1','0') AS positive_djt, IF(labeldjt='-1','1','0') AS negative_djt FROM task4_table")
+    #querytask6.show()
+    #reference https://stackoverflow.com/questions/38189088/convert-comma-separated-string-to-array-in-pyspark-dataframe
+    querytask6= querytask6.select(split(col("n_grams"), ",\s*").alias("n_grams"),col("positive_djt"),col("negative_djt")) #convert the "combined n_grams " from string form to actual array form
+
+    #reference: http://spark.apache.org/docs/2.2.0/api/python/pyspark.ml.html
+    cv = CountVectorizer(minDF=5.0, vocabSize=1 << 18, binary=True, inputCol="n_grams", outputCol="vectors")
+    model = cv.fit(querytask6)
+    task6Result = model.transform(querytask6)
+    task6Result.show(truncate=False) # for a better look of the table , remove "truncate=False"
 
 
 def modelfit():
@@ -156,6 +175,7 @@ def main(context):
     # YOU MAY ADD OTHER FUNCTIONS AS NEEDED
     task2()
     task4()
+    task6()
 
 
 if __name__ == "__main__":
