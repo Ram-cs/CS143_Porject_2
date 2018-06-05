90a65aab13151e779b9cdce2ea748c20e4270ca7
Ram-cs <ramusa2011@g.ucla.edu>
2018-06-04 23:14:12 -0700

    Task 8 done and Deliveberable

--- Pro2B/reddit_model.py
+++ Pro2B/reddit_model.py
@@ -219,9 +219,283 @@ if __name__ == "__main__":
     main(sqlContext)
 
 
-
+"""
 #Final Deliverable [[[JUST a ROUGH OUTLET OF CODE]]]
 ######## 1 ########
+
+## R CODE FOR GRAPHICS FOR
+## FINAL DELIVERABLE FOR CS143 PROJECT 2B
+
+# If you already have R installed on your home machine, transfer the resulting files
+# to your shared directory and do the visualizations in R on your home system rather than
+# in the VM because R is not installed there.
+
+# R is actually the simplest software for making plots.
+
+##################################################
+# PLOT 1: SENTIMENT OVER TIME (TIME SERIES PLOT)
+###################################################
+
+# This assumes you have a CSV file called "time_data.csv" with the columns:
+# date (like 2018-08-01), Positive, Negative
+# You should use the FULL PATH to the file, just in case.
+
+time.data <- read.csv("time_data.csv", stringsAsFactors=FALSE)
+time.data$date <- as.Date(time.data$date)
+# Fix a small data integrity issue from the data developer.
+time.data <- time.data[time.data$date != "2018-12-31", ]
+
+# Get start and end of time series.
+start <- min(time.data$date)
+end <- max(time.data$date)
+
+# Turn the data into a time series.
+positive.ts <- ts(time.data$Positive, start=start, end=end)
+negative.ts <- ts(time.data$Negative, start=start, end=end)
+
+# Plot it
+ts.plot(positive.ts, negative.ts, col=c("darkgreen", "red"),
+        gpars=list(xlab="Day", ylab="Sentiment", main="President Trump Sentiment on /r/politics Over Time"))
+
+################################################################
+# PLOT 2: SENTIMENT BY STATE (POSITIVE AND NEGATIVE SEPARATELY)
+################################################################
+
+# May need to do
+# install.packages("ggplot2)
+
+# This assumes you have a CSV file called "state_data.csv" with the columns:
+# state, Positive, Negative
+# You should use the FULL PATH to the file, just in case.
+
+library(ggplot2)
+library(dplyr)
+
+state.data <- read.csv("state_data.csv", header=TRUE)
+# rename it due to the format of the state data
+state.data$region <- state.data$state
+chloro <- state.data %>% mutate(region=tolower(region)) %>%
+    right_join(map_data("state"))
+
+ggplot(chloro, aes(long, lat)) +
+    geom_polygon(aes(group=group, fill=Positive)) +
+    coord_quickmap() +
+    scale_fill_gradient(low="#FFFFFF",high="#006400") +
+    ggtitle("Positive Trump Sentiment Across the US")
+
+ggplot(chloro, aes(long, lat)) +
+    geom_polygon(aes(group=group, fill=Negative)) +
+    coord_quickmap() +
+    scale_fill_gradient(low="#FFFFFF",high="#FF0000") +
+    ggtitle("Negative Trump Sentiment Across the US")
+
+################################################################
+# PLOT 3: SENTIMENT DIFF BY STATE
+################################################################
+chloro$Difference <- chloro$Positive - chloro$Negative
+ggplot(chloro, aes(long, lat)) +
+    geom_polygon(aes(group=group, fill=Difference)) +
+    coord_quickmap() +
+    scale_fill_gradient(low="#FFFFFF",high="#000000") +
+    ggtitle("Difference in Sentiment Across the US")
+
+##################################
+# PART 4 SHOULD BE DONE IN SPARK
+##################################
+
+########################################
+# PLOT 5A: SENTIMENT BY STORY SCORE
+########################################
+# What is the purpose of this? It helps us determine if the story score
+# should be a feature in the model. Remember that /r/politics is pretty
+# biased.
+
+# Assumes a CSV file called submission_score.csv with the following coluns
+# submission_score, Positive, Negative
+
+submission.data <- read.csv("submission_score.csv", quote="", header=TRUE)
+plot(Positive~story_score, data=story.data, col='darkgreen', pch='.',
+     main="Sentiment By Score on Submission")
+points(Negative~story_score, data=story.data, col='red', pch='.')
+
+
+########################################
+# PLOT 5B: SENTIMENT BY COMMENT SCORE
+########################################
+# What is the purpose of this? It helps us determine if the story score
+# should be a feature in the model. Remember that /r/politics is pretty
+# biased.
+
+# Assumes a CSV file called comment_score.csv with the following columns
+# comment_score, Positive, Negative
+
+comment.data <- read.csv("comment_score.csv", quote="", header=TRUE)
+plot(Positive~comment_score, data=comment.data, col='darkgreen', pch='.',
+     main="Sentiment By Score on Comments")
+points(Negative~comment_score, data=comment.data, col='red', pch='.')
+
+
+
+###############################
+# ANOTHER SAMPLE
+####################################
+# May first need:
+# In your VM: sudo apt-get install libgeos-dev (brew install on Mac)
+# pip3 install https://github.com/matplotlib/basemap/archive/v1.1.0.tar.gz
+
+import matplotlib.pyplot as plt
+import pandas as pd
+import datetime
+import numpy as np
+
+from mpl_toolkits.basemap import Basemap as Basemap
+from matplotlib.colors import rgb2hex
+from matplotlib.patches import Polygon
+
+"""
+    IMPORTANT
+    This is EXAMPLE code.
+    There are a few things missing:
+    1) You may need to play with the colors in the US map.
+    2) This code assumes you are running in Jupyter Notebook or on your own system.
+    If you are using the VM, you will instead need to play with writing the images
+    to PNG files with decent margins and sizes.
+    3) The US map only has code for the Positive case. I leave the negative case to you.
+    4) Alaska and Hawaii got dropped off the map, but it's late, and I want you to have this
+    code. So, if you can fix Hawaii and Alask, ExTrA CrEdIt. The source contains info
+    about adding them back.
+    """
+
+
+"""
+    PLOT 1: SENTIMENT OVER TIME (TIME SERIES PLOT)
+    """
+# Assumes a file called time_data.csv that has columns
+# date, Positive, Negative. Use absolute path.
+
+ts = pd.read_csv("time_data.csv")
+# Remove erroneous row.
+ts = ts[ts['date'] != '2018-12-31']
+
+plt.figure(figsize=(12,5))
+ts.date = pd.to_datetime(ts['date'], format='%Y-%m-%d')
+ts.set_index(['date'],inplace=True)
+
+ax = ts.plot(title="President Trump Sentiment on /r/politics Over Time",
+             color=['green', 'red'],
+             ylim=(0, 1.05))
+ax.plot()
+
+"""
+    PLOT 2: SENTIMENT BY STATE (POSITIVE AND NEGATIVE SEPARATELY)
+    # This example only shows positive, I will leave negative to you.
+    """
+
+# This assumes you have a CSV file called "state_data.csv" with the columns:
+# state, Positive, Negative
+#
+# You should use the FULL PATH to the file, just in case.
+
+state_data = pd.read_csv("state_data.csv")
+
+"""
+    You also need to download the following files. Put them somewhere convenient:
+    https://github.com/matplotlib/basemap/blob/master/examples/st99_d00.shp
+    https://github.com/matplotlib/basemap/blob/master/examples/st99_d00.dbf
+    https://github.com/matplotlib/basemap/blob/master/examples/st99_d00.shx
+    """
+
+# Lambert Conformal map of lower 48 states.
+m = Basemap(llcrnrlon=-119, llcrnrlat=22, urcrnrlon=-64, urcrnrlat=49,
+            projection='lcc', lat_1=33, lat_2=45, lon_0=-95)
+shp_info = m.readshapefile('/path_to/st99_d00','states',drawbounds=True)  # No extension specified in path here.
+pos_data = dict(zip(state_data.state, state_data.Positive))
+neg_data = dict(zip(state_data.state, state_data.Negative))
+
+# choose a color for each state based on sentiment.
+pos_colors = {}
+statenames = []
+pos_cmap = plt.cm.Greens # use 'hot' colormap
+
+vmin = 0; vmax = 1 # set range.
+for shapedict in m.states_info:
+    statename = shapedict['NAME']
+    # skip DC and Puerto Rico.
+    if statename not in ['District of Columbia', 'Puerto Rico']:
+        pos = pos_data[statename]
+        pos_colors[statename] = pos_cmap(1. - np.sqrt(( pos - vmin )/( vmax - vmin)))[:3]
+    statenames.append(statename)
+# cycle through state names, color each one.
+
+# POSITIVE MAP
+ax = plt.gca() # get current axes instance
+for nshape, seg in enumerate(m.states):
+    # skip Puerto Rico and DC
+    if statenames[nshape] not in ['District of Columbia', 'Puerto Rico']:
+        color = rgb2hex(pos_colors[statenames[nshape]])
+        poly = Polygon(seg, facecolor=color, edgecolor=color)
+        ax.add_patch(poly)
+plt.title('Positive Trump Sentiment Across the US')
+plt.show()
+
+
+# SOURCE: https://stackoverflow.com/questions/39742305/how-to-use-basemap-python-to-plot-us-with-50-states
+# (this misses Alaska and Hawaii. If you can get them to work, EXTRA CREDIT)
+
+"""
+    PART 4 SHOULD BE DONE IN SPARK
+    """
+
+"""
+    PLOT 5A: SENTIMENT BY STORY SCORE
+    """
+# What is the purpose of this? It helps us determine if the story score
+# should be a feature in the model. Remember that /r/politics is pretty
+# biased.
+
+# Assumes a CSV file called submission_score.csv with the following coluns
+# submission_score, Positive, Negative
+
+story = pd.read_csv("submission_score.csv")
+plt.figure(figsize=(12,5))
+fig = plt.figure()
+ax1 = fig.add_subplot(111)
+
+ax1.scatter(story['submission_score'], story['Positive'], s=10, c='b', marker="s", label='Positive')
+ax1.scatter(story['submission_score'], story['Negative'], s=10, c='r', marker="o", label='Negative')
+plt.legend(loc='lower right');
+
+plt.xlabel('President Trump Sentiment by Submission Score')
+plt.ylabel("Percent Sentiment")
+plt.show()
+
+"""
+    PLOT 5B: SENTIMENT BY COMMENT SCORE
+    """
+# What is the purpose of this? It helps us determine if the comment score
+# should be a feature in the model. Remember that /r/politics is pretty
+# biased.
+
+# Assumes a CSV file called comment_score.csv with the following columns
+# comment_score, Positive, Negative
+
+story = pd.read_csv("comment_score.csv")
+plt.figure(figsize=(12,5))
+fig = plt.figure()
+ax1 = fig.add_subplot(111)
+
+ax1.scatter(story['comment_score'], story['Positive'], s=10, c='b', marker="s", label='Positive')
+ax1.scatter(story['comment_score'], story['Negative'], s=10, c='r', marker="o", label='Negative')
+plt.legend(loc='lower right');
+
+plt.xlabel('President Trump Sentiment by Comment Score')
+plt.ylabel("Percent Sentiment")
+plt.show()
+
+"""
+
+
+
 #Create a time series plot of positive and negative sentiment. This plot should contain two lines,
 # one for positive and one for negative. It must have data as an X axis and the percentage of comments
 # classified as each sentiment on the Y axis.
@@ -287,3 +561,5 @@ plt.show() #showing graph
 ######## 8 ##########
 #Write a paragraph summarizing your findings. What does /r/politics think about President Trump? Does this vary by state? Over time? By story/submission?
 """
+
+
