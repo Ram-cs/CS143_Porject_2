5739fd44ba7813281fe42c63a1799badbcd23edd
Ram-cs <ramusa2011@g.ucla.edu>
2018-06-04 23:05:58 -0700

    task 8 done

--- Pro2B/reddit_model.py
+++ Pro2B/reddit_model.py
@@ -178,32 +178,31 @@ def task8():
     #1
     comments.createOrReplaceTempView("comment_data")
     sqlDF = spark.sql("SELECT created_utc as comment_timestamp FROM comment_data")
-#    sqlDF.show() #debugging purpose
-    sqlDF.write.saveAsTable("task8_timestamp")
+    sqlDF.show() #debugging purpose
+    #sqlDF.write.saveAsTable("task8_timestamp")
 
     #2
     submissions.createOrReplaceTempView("submission_data")
-    sqlDF_submission = spark.sql("SELECT title FROM comment_data JOIN submission_data ON CONCAT('t3_',comment_data.link_id) = submission_data.id")
-#    sqlDF_submission.show() #debugging purpose
-    sqlDF_submission.write.saveAsTable("task8_timestamp")
+    sqlDF_submission = spark.sql("SELECT title FROM comment_data JOIN submission_data ON (Replace(comment_data.link_id, 't3_', '')) = submission_data.id")
+    sqlDF_submission.show() #debugging purpose
+    #sqlDF_submission.write.saveAsTable("task8_timestamp")
 
     #3
     sqlDF_3 = spark.sql("SELECT author_flair_text as state FROM submission_data")
-#    sqlDF_3.show() #debuggine purpose
-    sqlDF_3.write.saveAsTable("task8_state")
+    sqlDF_3.show() #debuggine purpose
+    #sqlDF_3.write.saveAsTable("task8_state")
 
-
+#task 9
 
 
 def main(context):
     """Main function takes a Spark SQL context."""
     # YOUR CODE HERE
     # YOU MAY ADD OTHER FUNCTIONS AS NEEDED
-#    task2()
-#    task4()
-#    task6()
-#    modelfit()
-    print("*****************")
+    task2()
+    task4()
+    task6()
+    modelfit()
     task8()
 
 
